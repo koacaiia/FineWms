87d5a742324178b7fdb045977a1fae5064348717
AdroidYsk <koacaiia@gamil.com>
2021-06-08 17:46:39 +0900

    0608_2

--- app/src/main/java/fine/koacaiia/finewms/IncargoListAdapter.java
+++ app/src/main/java/fine/koacaiia/finewms/IncargoListAdapter.java
@@ -1,5 +1,6 @@
 package fine.koacaiia.finewms;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -15,20 +16,58 @@ import java.util.ArrayList;
 public class IncargoListAdapter extends RecyclerView.Adapter<IncargoListAdapter.ListViewHolder>{
     ArrayList<IncargoList> list;
 
+    public interface IncargoListAdapterOnClickListener{
+        void incargoOnClick(IncargoListAdapter.ListViewHolder listViewHolder,View v,int position);
 
-    public IncargoListAdapter(ArrayList<IncargoList> list) {
+    }
+    public interface IncargoListAdapterOnLongClickListener{
+        void incargoOnLongClick(IncargoListAdapter.ListViewHolder listViewHolder,View v,int position);
+    }
+
+    IncargoListAdapterOnClickListener clickListener;
+    IncargoListAdapterOnLongClickListener longClickListener;
+
+    public IncargoListAdapter(ArrayList<IncargoList> list,IncargoListAdapterOnClickListener clickListener,
+                              IncargoListAdapterOnLongClickListener longClickListener) {
         this.list=list;
+        this.clickListener=clickListener;
+        this.longClickListener=longClickListener;
     }
 
     @NonNull
     @NotNull
     @Override
     public ListViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.incargolist,parent,false);
+
+        return new ListViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull ListViewHolder holder, int position) {
+        String str_container20=list.get(position).getContainer20();
+        String str_container40=list.get(position).getContainer40();
+        String str_lclcargo=list.get(position).getLclcargo();
+        String cargoType;
+        if(str_container20.equals("1")){
+            cargoType="20FT";
+        }else if(str_container40.equals("1")){
+            cargoType="40FT";
+        }else if(str_lclcargo.equals("1")){
+            cargoType="Cargo";
+        }else{
+            cargoType="미정";
+        }
+
+        holder.working.setText(list.get(position).getWorking());
+        holder.date.setText(list.get(position).getDate());
+        holder.consignee.setText(list.get(position).getConsignee());
+        holder.container.setText(list.get(position).getContainer());
+        holder.cargotype.setText(cargoType);
+        holder.remark.setText(list.get(position).getRemark());
+        holder.bl.setText(list.get(position).getBl());
+        holder.des.setText(list.get(position).getDescription());
+        holder.incargo.setText(list.get(position).getIncargo());
 
     }
 
@@ -50,6 +89,30 @@ public class IncargoListAdapter extends RecyclerView.Adapter<IncargoListAdapter.
         LinearLayout cardView;
         public ListViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
+            this.working=itemView.findViewById(R.id.incargo_working);
+            this.date=itemView.findViewById(R.id.incargo_date);
+            this.container=itemView.findViewById(R.id.incargo_container);
+            this.consignee=itemView.findViewById(R.id.incargo_consignee);
+            this.cargotype=itemView.findViewById(R.id.incargo_cargotype);
+            this.remark=itemView.findViewById(R.id.incargo_remark);
+            this.bl=itemView.findViewById(R.id.incargo_bl);
+            this.des=itemView.findViewById(R.id.incargo_des);
+            this.incargo=itemView.findViewById(R.id.incargo_incargo);
+            this.cardView=itemView.findViewById(R.id.incargo_back);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickListener.incargoOnClick(ListViewHolder.this,v,getAdapterPosition());
+                }
+            });
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    longClickListener.incargoOnLongClick(ListViewHolder.this,v,getAdapterPosition());
+                    return true;
+                }
+            });
 
         }
     }
--- app/src/main/java/fine/koacaiia/finewms/MainActivity.java
+++ app/src/main/java/fine/koacaiia/finewms/MainActivity.java
@@ -7,6 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,11 +20,13 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.annotations.NotNull;
 
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IncargoListAdapter.IncargoListAdapterOnClickListener, IncargoListAdapter.IncargoListAdapterOnLongClickListener {
     RecyclerView recyclerView;
     ArrayList<IncargoList> list;
+    String[] consigneeList;
     IncargoListAdapter adapter;
     FirebaseDatabase database;
     String depotName;
@@ -37,22 +43,32 @@ public class MainActivity extends AppCompatActivity {
         depotName="Incargo2";
         list=new ArrayList<>();
         getIncargoData();
-        adapter=new IncargoListAdapter(list);
+        adapter=new IncargoListAdapter(list,this,this);
+        recyclerView.setAdapter(adapter);
+
+
+
     }
 
     private void getIncargoData() {
-        Log.i("duatjsrb","Firebase Data get"+depotName);
-//        list.clear();
+        list.clear();
+        ArrayList<String> consigneeArrayList=new ArrayList<>();
         DatabaseReference databaseReference=database.getReference("IncargoData/Department/"+depotName);
-
         ValueEventListener valueEventListener=new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
                 for(DataSnapshot data:snapshot.getChildren()){
                     IncargoList mList=data.getValue(IncargoList.class);
+                    list.add(mList);
+                    if(!consigneeArrayList.contains(mList.getConsignee())){
+                        consigneeArrayList.add(mList.getConsignee());
+                    }
+                   }
 
-                    Log.i("duatjsrb","consignee Value"+mList.getConsignee());
-                }
+                consigneeList=consigneeArrayList.toArray(new String[consigneeArrayList.size()]);
+                adapter.notifyDataSetChanged();
+
+
 
             }
 
@@ -62,5 +78,24 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         databaseReference.addListenerForSingleValueEvent(valueEventListener);
+    }
+
+
+
+    @Override
+    public void incargoOnClick(IncargoListAdapter.ListViewHolder listViewHolder, View v, int position) {
+        Toast.makeText(this,list.get(position).getBl()+"bl:SELECTED",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void incargoOnLongClick(IncargoListAdapter.ListViewHolder listViewHolder, View v, int position) {
+        Toast.makeText(this,list.get(position).getConsignee()+"Consignee:SELECTED",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
